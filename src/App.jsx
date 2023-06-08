8ead7202edaa17ad151ae7183eed12072be87d5f
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2023-06-08 20:34:32 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout.jsx';
+import Home from './components/views/Home.jsx';
+import Modules from './components/views/Modules.jsx';
+import Students from './components/views/Students.jsx';
+
+function App() {
+  const loggedInUser = 'Graeme';
+
+  return (
+    <BrowserRouter>
+      <Layout loggedInUser={loggedInUser}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/modules' element={<Modules />} />
+          <Route path='/students' element={<Students />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
